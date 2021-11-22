afeb1581d5d05749aa4941cb411ff4e4b3c9570a
Sanpeta <sidneisonic@hotmail.com>
2021-11-22 13:42:29 -0300

    Initial Commit

---
+++ ioasys/ioasysApp.swift
@@ -0,0 +1,17 @@
+//
+//  ioasysApp.swift
+//  ioasys
+//
+//  Created by Sidnei de Souza Junior on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ioasysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
