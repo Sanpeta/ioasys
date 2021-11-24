c86be9499d74bb3c834cd2eac7e9a0b480fd4687
Sanpeta <sidneisonic@hotmail.com>
2021-11-23 23:03:43 -0300

    [LoginView && KeyboardHandler] Foi ajustado a tela conforme o keyboard aparece na tela, assim não deixando oculto o botão ou qualquer elemento da tela.

--- ioasys/Views/LoginView.swift
+++ ioasys/Views/LoginView.swift
@@ -15,62 +15,62 @@ struct LoginView: View {
     @State private var statusPass = false
     @State private var statusButton = false
     @State private var keyboardHeight: CGFloat = 0
-    
+    @StateObject private var keyboardHandle = KeyboardHandler()
     
     //MARK: - Body
     var body: some View {
         
         NavigationView {
-            VStack {
-                    ZStack(alignment: .bottomLeading) {
-                        Image("BackgroundLogin")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(height: UIScreen.main.bounds.height*0.6)
-                        
-                        VStack(alignment: .leading) {
-                            Text("Boas vindas,")
-                                .font(.system(size: 38, weight: .bold, design: .monospaced))
-                                .foregroundColor(.white)
-                                
-                            
-                            Text("Você está no Empresas.")
-                                .font(.system(size: 24, weight: .light, design: .monospaced))
-                                .foregroundColor(.white)
-                                
-                        }//: VStack
-                        .padding([.bottom, .leading], 24)
-                    }
-                    .ignoresSafeArea(.all)
+            VStack(spacing: 0) {
+                ZStack(alignment: .bottomLeading) {
+                    Image("BackgroundLogin")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: UIScreen.main.bounds.height*0.5)
                     
-                    VStack(alignment: .leading, spacing: 24) {
-                        Text("Digite seus dados para continuar.")
-                            .foregroundColor(.black)
-                            .font(.system(size: 16, weight: .semibold, design: .default))
-                            .padding(.leading, 24)
-                            .padding(.top, 16)
+                    VStack(alignment: .leading) {
+                        Text("Boas vindas,")
+                            .font(.system(size: 38, weight: .bold, design: .monospaced))
+                            .foregroundColor(.white)
+                            
                         
-                        CustomTextField(text: $email, status: $statusEmail, buttonType: .email)
-                        
-                        CustomTextField(text: $password, status: $statusPass, buttonType: .password)
-                        
-                        NavigationLink(destination: HomeView()) {
-                            Text("ENTRAR")
-                                .font(.system(size: 16, weight: .semibold, design: .monospaced))
-                                .foregroundColor(.white)
-                                .frame(width: UIScreen.main.bounds.width - 48, height: 50)
-                                .background(statusButton ? .gray : .black)
-                                .clipShape(Capsule())
-                                .disabled(statusButton)
-                                .padding(.leading, 24)
-                        }
+                        Text("Você está no Empresas.")
+                            .font(.system(size: 24, weight: .light, design: .monospaced))
+                            .foregroundColor(.white)
+                            
                     }//: VStack
-                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45, alignment: .top)
-                    .background(Color.white)
-
+                    .padding(.leading, 24)
+                    .offset(y: 24)
+                }
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    Text("Digite seus dados para continuar.")
+                        .foregroundColor(.black)
+                        .font(.system(size: 16, weight: .semibold, design: .default))
+                        .padding(.leading, 24)
+                        .padding(.top, 24)
+                    
+                    CustomTextField(text: $email, status: $statusEmail, buttonType: .email)
+                    
+                    CustomTextField(text: $password, status: $statusPass, buttonType: .password)
+                        
+                    NavigationLink(destination: HomeView()) {
+                        Text("ENTRAR")
+                            .font(.system(size: 16, weight: .semibold, design: .monospaced))
+                            .foregroundColor(.white)
+                            .frame(width: UIScreen.main.bounds.width - 48, height: 50)
+                            .background(statusButton ? .gray : .black)
+                            .clipShape(Capsule())
+                            .disabled(statusButton)
+                            .padding(.leading, 24)
+                    }
                 }//: VStack
-            .ignoresSafeArea(.keyboard)
-        }
+                .background(Color.white)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45, alignment: .center)
+            }//: VStack
+            .padding(.bottom, 48)
+            .padding(.bottom, keyboardHandle.keyboardHeight)
+        }//: NavigationView
     }
 }
 
