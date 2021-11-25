9c74754996f9de77e9cd429a68f94bc09cf7a79e
Sanpeta <sidneisonic@hotmail.com>
2021-11-25 11:29:32 -0300

    [Constants] Criado as rotas para cada situacao.

--- ioasys/Utils/Constants.swift
+++ ioasys/Utils/Constants.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+let URL_ENTERPRISES = "https://empresas.ioasys.com.br/api/v1/enterprises"
+
+let URL_LOGIN = "https://empresas.ioasys.com.br/api/v1/users/auth/sign_in"
+
+let URL_IMAGE = "https://empresas.ioasys.com.br/"
