fbbd79fd2eb87940a3addc5bcbfefe7d4fd46ef8
Sanpeta <sidneisonic@hotmail.com>
2021-11-25 11:47:56 -0300

    [LoginView] Adicionado authViewModel para realizar comunicacao com suas funcionalidades e atributos.

--- ioasys/Views/LoginView.swift
+++ ioasys/Views/LoginView.swift
@@ -9,68 +9,73 @@ import SwiftUI
 
 struct LoginView: View {
     //MARK: - Properties
-    @State private var email = ""
-    @State private var password = ""
-    @State private var statusEmail = false
-    @State private var statusPass = false
-    @State private var statusButton = false
     @State private var keyboardHeight: CGFloat = 0
     @StateObject private var keyboardHandle = KeyboardHandler()
+    @StateObject private var authViewModel = AuthenticationViewModel()
     
     //MARK: - Body
     var body: some View {
         
-        NavigationView {
-            VStack(spacing: 0) {
-                ZStack(alignment: .bottomLeading) {
-                    Image("BackgroundLogin")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(height: UIScreen.main.bounds.height*0.5)
-                    
-                    VStack(alignment: .leading) {
-                        Text("Boas vindas,")
-                            .font(Font.custom("Gilroy-ExtraBold", size: 42))
-                            .foregroundColor(.white)
+        Group {
+            //Verificando se o usuario realizou o login
+            if authViewModel.statusLogin {
+                HomeView()
+            } else {
+                NavigationView {
+                    VStack(spacing: 0) {
+                        ZStack(alignment: .bottomLeading) {
+                            Image("BackgroundLogin")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: UIScreen.main.bounds.height * 0.40)
                             
-                        
-                        Text("Você está no Empresas.")
-                            .font(Font.custom("Gilroy-Light", size: 24))
-                            .foregroundColor(.white)
-                            
-                    }//: VStack
-                    .padding(.leading, 24)
-                    .offset(y: 24)
-                }
-                
-                VStack(alignment: .leading, spacing: 24) {
-                    Text("Digite seus dados para continuar.")
-                        .foregroundColor(.black)
-                        .font(Font.custom("Gilroy-ExtraBold", size: 15))
-                        .padding(.leading, 24)
-                        .padding(.top, 24)
-                    
-                    CustomTextField(text: $email, status: $statusEmail, buttonType: .email)
-                    
-                    CustomTextField(text: $password, status: $statusPass, buttonType: .password)
-                        
-                    NavigationLink(destination: HomeView()) {
-                        Text("ENTRAR")
-                            .font(.system(size: 16, weight: .semibold, design: .monospaced))
-                            .foregroundColor(.white)
-                            .frame(width: UIScreen.main.bounds.width - 48, height: 50)
-                            .background(statusButton ? .gray : .black)
-                            .clipShape(Capsule())
-                            .disabled(statusButton)
+                            VStack(alignment: .leading) {
+                                Text("Boas vindas,")
+                                    .font(Font.custom("Gilroy-ExtraBold", size: 42))
+                                    .foregroundColor(.white)
+                                    
+                                
+                                Text("Você está no Empresas.")
+                                    .font(Font.custom("Gilroy-Light", size: 24))
+                                    .foregroundColor(.white)
+                                    
+                            }//: VStack
                             .padding(.leading, 24)
-                    }
-                }//: VStack
-                .background(Color.white)
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45, alignment: .center)
-            }//: VStack
-            .padding(.bottom, 48)
-            .padding(.bottom, keyboardHandle.keyboardHeight)
-        }//: NavigationView
+                            .padding(.bottom, 24)
+                        }//: ZStack
+                        
+                        VStack(alignment: .leading, spacing: 24) {
+                            Text("Digite seus dados para continuar.")
+                                .foregroundColor(.black)
+                                .font(Font.custom("Gilroy-ExtraBold", size: 15))
+                                .padding(.leading, 24)
+                                .padding(.top, 24)
+                            
+                            CustomTextField(text: $authViewModel.email, status: $authViewModel.statusEmail, buttonType: .email)
+                            
+                            CustomTextField(text: $authViewModel.password, status: $authViewModel.statusPass, buttonType: .password)
+                                
+                            Button {
+                                authViewModel.login(authViewModel.email, authViewModel.password)
+                            } label: {
+                                Text("ENTRAR")
+                                    .font(.system(size: 16, weight: .semibold, design: .monospaced))
+                                    .foregroundColor(.white)
+                                    .frame(width: UIScreen.main.bounds.width - 48, height: 50)
+                                    .background(authViewModel.buttonBackgroundColor)
+                                    .clipShape(Capsule())
+                                    .padding(.leading, 24)
+                            }
+                            .disabled(!authViewModel.formIsValid)
+
+                        }//: VStack
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.4, alignment: .topLeading)
+                        .background(Color.white)
+                    }//: VStack
+                    .padding(.bottom, keyboardHandle.keyboardHeight)
+                }//: NavigationView
+            }//: ELSE
+        }//: Group
     }
 }
 
