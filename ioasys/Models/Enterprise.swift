87e0e8cf1b93768fc7d0d77748e39f7fef16b865
Sanpeta <sidneisonic@hotmail.com>
2021-11-25 12:07:31 -0300

    [Enterprise] Criado o modelo para empresa.

--- ioasys/Models/Enterprise.swift
+++ ioasys/Models/Enterprise.swift
@@ -1,5 +1,5 @@
 //
-//  Enterprises.swift
+//  Enterprise.swift
 //  ioasys
 //
 //  Created by Sidnei de Souza Junior on 24/11/21.
@@ -8,7 +8,18 @@
 import Foundation
 
 struct Enterprises: Codable {
+    let enterprises: [Enterprise]
+}
+
+struct Enterprise: Hashable, Codable {
     let id: Int
     let enterprise_name: String
     let photo: String
+    let description: String
+    let enterprise_type: EnterpriseType
+}
+
+struct EnterpriseType: Hashable, Codable {
+    let id: Int
+    let enterprise_type_name: String
 }
